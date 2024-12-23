e1c2749ec52189541662c483e968a180a4e76dd6
Longxi Zhou <longxi.zhou@kaust.edu.sa>
2024-12-23 15:51:57 +0300

    Delete smooth_mask directory

--- smooth_mask/visualize/visualize_basic_semantics.py
+++
@@ -1,7 +0,0 @@
-from chest_ct_database.visualize_manager.add_basic_tissue_visualize import add_visualization_basic_semantic
-
-if __name__ == '__main__':
-    add_visualization_basic_semantic(
-        '/data_disk/artery_vein_project/extract_blood_region/rescaled_ct-denoise/',
-        '/data_disk/artery_vein_project/extract_blood_region/semantics/',
-        '/data_disk/artery_vein_project/extract_blood_region/visualization/basic_semantic_check/', fold=(0, 1))
