ee73290ba4a604a061ae3eed6beddca95992f94f
Longxi Zhou <longxi.zhou@kaust.edu.sa>
2024-12-23 15:52:09 +0300

    Delete visualization directory

--- visualization/video/generate_video.py
+++
@@ -1,9 +0,0 @@
-import imageio
-import os
-top_dict = '/data_disk/pulmonary_embolism_final/pictures/segmentation_visualization'
-images = []
-fn_list = os.listdir(top_dict)
-fn_list.sort()
-for filename in fn_list:
-    images.append(imageio.imread(os.path.join(top_dict, filename)))
-imageio.mimsave('/data_disk/pulmonary_embolism_final/pictures/movie_seg.gif', images, duration=0.25)
